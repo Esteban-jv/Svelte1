a97545001deabb83a65b8a856bc329b1efbce161
Esteban-jv <ejv.developer@gmail.com>
2025-01-27 22:20:53 -0500

    First commit (Reactivity)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+    let number = $state(0);
+    // let stringShow = $derived(number === 0 ? 'Why not try to click the button below?' : `You've clicked ${number} times`);
+    let stringShow = $derived.by(() => calculateUserInformation(number)); // Mientras no usemos nada de $derived o otras cosas de svelte, podríamos no usar $state
+
+    const onclick = () => {
+        number ++;
+    }
+    function calculateUserInformation (number: number) {
+        if (number === 0) {
+            return 'Why not try to click the button below?';
+        } else if (number === 1) {
+            return `You've clicked exactly one time`;
+        }
+        return `You've clicked ${number} times`;
+    }
+</script>
+<h1 class="text-2xl">{number}</h1>
+<button class="bg-green-400 p-2 text-gray-700 rounded-md hover:bg-green-600" {onclick}>Click me</button>
+<div>
+    <p>{ stringShow }</p>
+</div>
