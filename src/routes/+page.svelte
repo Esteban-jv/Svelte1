8a50f3bc43236a0d0010cb25c4ba19bafe201273
Esteban-jv <ejv.developer@gmail.com>
2025-02-01 12:26:28 -0500

    test two

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-
+    import PageTwo from "./pageTwo.svelte";
     let number = $state(0);
     // let stringShow = $derived(number === 0 ? 'Why not try to click the button below?' : `You've clicked ${number} times`);
     let stringShow = $derived.by(() => calculateUserInformation(number)); // Mientras no usemos nada de $derived o otras cosas de svelte, podríamos no usar $state
@@ -15,9 +15,16 @@
         }
         return `You've clicked ${number} times`;
     }
+
+    // -----------------------------------------------
+    let userName = $state("")
 </script>
 <h1 class="text-2xl">{number}</h1>
 <button class="bg-green-400 p-2 text-gray-700 rounded-md hover:bg-green-600" {onclick}>Click me</button>
 <div>
     <p>{ stringShow }</p>
 </div>
+<input type="text" name="userName" id="userName" bind:value={userName} />
+<p>{ userName }</p>
+
+<PageTwo />
---
+++ src/routes/pageTwo.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let peopleWaiting = $state(['Esteban', 'Juan']);
+    $inspect(peopleWaiting);
+</script>
+
+<h1 class="text-2xl">Page Two</h1>
+{#snippet userInput(personId: string)}
+    <li>Id for person waiting: {personId}</li>
+{/snippet}
+
+<ul>
+    {#each peopleWaiting as person}
+        {@render userInput(person)}
+    {/each}
+</ul>
+
+<button
+    onclick="{() => peopleWaiting.push(new Date().toLocaleTimeString())}"
+>I'm waiting too</button>
